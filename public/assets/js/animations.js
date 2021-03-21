a9dde2b21d9d1bc22fe05207f00edd1e65fd7360
kuromi-ctrl <74784652+kuromi-ctrl@users.noreply.github.com>
2021-03-21 00:26:21 -0300

    attat
    attatt

---
+++ public/assets/js/animations.js
@@ -0,0 +1,7 @@
+var button = document.getElementById('botao')
+var menu = document.querySelector('.menu-ul')
+
+button.addEventListener('click',function(){
+    menu.classList.toggle('menu-aberto')
+    
+})
